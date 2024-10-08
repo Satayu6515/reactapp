38fb2c05ae956c6805147245c5142a4492e06639
Satayu6515 <162657143+Satayu6515@users.noreply.github.com>
2024-10-08 16:22:56 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,78 @@
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes, Outlet, Navigate, useLocation } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/Users';
+import Account from './pages/Account';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Register from './pages/register';
+import Login from './pages/Login';
+import { AuthProvider, useAuth } from './AuthProvider';
+import MyBlog from './pages/myblog';
+import AddBlog from './pages/addblog';
+import EditBlog from './pages/editblog';
+
+
+const LayoutAdmin = () => {
+
+  return (
+    <ProtectedRoute>
+     <div className="flex">
+     <Sidebar />
+     <div className="flex-1">
+      <Header />
+      <div className="m-6 p-10 bg-gray-50 min-h-screen rounded-lg">
+       <Outlet /> {/* Outlet  คือส่วนที่จะแสดงคอมโพเน้นลูกในธีมของแอดมิน */}
+      </div>
+     </div>
+    </div>
+    </ProtectedRoute>
+
+   );
+ 
+ }
+
+ const ProtectedRoute = ({ children }) => {
+
+  const { user } = useAuth();
+ 
+  const location = useLocation();
+ 
+ 
+ 
+  if (!user) {
+ 
+   return <Navigate to="/login" state={{ from: location }} replace />;
+ 
+  }
+ 
+  return children;
+ 
+ };
+
 
 function App() {
   return (
-      <BrowserRouter>
-        <div className='flex'>
-          <Sidebar />
-          <div className='flex-1'>
-            <Header />
-            <Routes>
-              <Route path='/Dashboard' element={<Dashboard />} />
-              <Route path='/users' element={<Users />} />
-              <Route path='/About' element={<About />} />
-
-              
+    <AuthProvider>
+              <BrowserRouter>
+              <Routes>
+                <Route element={<LayoutAdmin />}>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/Account' element={<Account />} />
+                <Route path='/users' element={<Users />} />
+                <Route path='/About' element={<About />} />
+                <Route path='/myblog' element={<MyBlog />} />
+                <Route path='/new-post' element={<AddBlog/>} />
+                <Route path='/edit-post/:id' element={<EditBlog/>} />
+        </Route>
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
         </Routes>
-          </div>
-        </div>
       </BrowserRouter> 
+    </AuthProvider>
+
   );
 }
 
